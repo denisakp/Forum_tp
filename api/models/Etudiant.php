034456f2c7b3d84c576bcd90ca6c5a6751cae97f
denisakp <akpagnonited@gmail.com>
2019-02-14 14:30:41 +0000

    Adding class doc, adding readOneEtudiant function end editing addEtudiant

--- api/models/Etudiant.php
+++ api/models/Etudiant.php
@@ -1,15 +1,44 @@
 <?php
 
 class Etudiant {
-
+    /**
+     * @var string Table utilisée pour effectuer les opérations avec la BDD
+     */
     private $table = 'etudiant';
 
+    /**
+     * @var string le numéro matricule de l'etudiant
+     */
     private $matricule;
+
+    /**
+     * @var string Le nom de l'étudiant
+     */
     private $nom;
+
+    /**
+     * @var string le prenom de l'étudiant
+     */
     private $prenom;
+
+    /**
+     * @var string le pseudo de l'étudiant
+     */
     private $pseudo;
+
+    /**
+     * @var string le mot de passe de l'étudiant
+     */
     private $motdepasse;
+
+    /**
+     * @var string La filiere de l'etudiant 
+     */
     private $filiere;
+
+    /**
+     * Constructeur de la classe Etudiant
+     */
 
     public function __construct($nom, $prenom, $pseudo, $motdepasse, $filiere){
         $this->nom = $nom;
@@ -27,22 +56,39 @@ class Etudiant {
         $this->$property = $value;
     }
 
+    /**
+     * Getter de la proprieté nom
+     */
     public function getNom(){
         return $this->nom;
     }
 
+    /**
+     * Getter de la proprieté prenom
+     */
     public function getPrenom(){
         return $this->prenom;
     }
+
+    /**
+     * Getter de la proprieté Pseudo
+     */
 
     public function getPseudo(){
         return $this->pseudo;
     }
 
+    /**
+     * GEtter de la proprieté Motdepasse
+     */
     public function getMotdepasse(){
         return $this->motdepasse;
     }
 
+
+    /**
+     * Getter de la proprieté filière
+     */
     public function getFiliere(){
         return $this->filiere;
     }
@@ -68,6 +114,36 @@ class Etudiant {
             return false;
         }
     } 
+
+    /**
+     * Fonction Liste
+     * Cette fonction permet d'afficher une seule categorie
+     * 
+     * $con:il s'agit de la valeur retournée par la méthode connect de la classe Database
+     * 
+     * $sql: est la requete SQL qui sera exécuté
+     * 
+     * Si aucune erreur, elle retourne true, sinon elle retourne false
+     * @return boolean
+     */
+    public function readOneEtudiant(Etudiant $etudiant, $matricule){
+        $con = Database::connect();
+        $sql = 'SELECT * FROM '.$this->table.' WHERE matricule = ? ';
+
+        $stmt = $con->prepare($sql);
+        $smt-> binParam(1, $this->matricule);
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $this->matricule = $row['matricule'];
+        $this->nom = $row['nom'];
+        $this->prenom = $row['prenom'];
+        $this->pseudo = $row['pseudo'];
+        $this->motdepasse = $row['motdepasse'];
+        $this->filiere = $row['filiere'];
+
+        return true;
+    }
 
     public function addEtudiant(Etudiant $etudiant){
         $con = Database::connect();
@@ -97,9 +173,6 @@ class Etudiant {
             printf("Erreur: $s.\n", $stmt->error);
             return false;
         }
-
-
-
 
         $res = $stmt->execute();
         if($res){

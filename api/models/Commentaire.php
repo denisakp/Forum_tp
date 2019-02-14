92745e54d40477d14e16b4a2150297ad58db0ece
denisakp <akpagnonited@gmail.com>
2019-02-14 15:19:51 +0000

    Create  comentaire class, setting the properties and the getters

---
+++ api/models/Commentaire.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Class Commentaire
+ */
+
+ class commentaire{
+    private $table = "commentaire";
+
+     private $id_com;
+     private $contenu_disc;
+     private $date_com;
+     private $matricule;
+     private $id_disc;
+
+     public function __construct(){}
+
+     public function __set($property, $value){
+        $this->$property = $value;
+    }
+
+     public function __get($property){
+        return $this->$property;
+    }
+
+     public function getID_con(){
+         return $this->getID_con;
+     }
+
+     public function getContenu_Disc(){
+         return $this->contenu_disc;
+     }
+
+     public function getMatricule(){
+         return $this->matricule;
+     }
+
+     public function getDate_com(){
+         return $this->date_com;
+     }
+
+     public function getId_disc(){
+         return $this->id_disc;
+     }
+
+
+     
+
+
+     
+ }

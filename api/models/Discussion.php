4d9c00f0bd456d3acb5645edff5867ae895798eb
denisakp <akpagnonited@gmail.com>
2019-02-14 17:49:07 +0000

    adding Discussion class properties

--- api/models/Discussion.php
+++ api/models/Discussion.php
@@ -4,5 +4,12 @@
  */
 
  class Discussion{
-     
+     private $table = "discussion";
+
+     private $id_disc;
+     private $titre_disc;
+     private $contenu_disc;
+     private $date_disc;
+     private $etat_disc;
+     private $id_cat;
  }

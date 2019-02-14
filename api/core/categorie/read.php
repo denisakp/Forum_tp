a9d035a28a508f358f12919020294ea32a5b2686
denisakp <akpagnonited@gmail.com>
2019-02-14 23:05:28 +0000

    Improve the code

--- api/core/categorie/read.php
+++ api/core/categorie/read.php
@@ -5,7 +5,7 @@
   include_once '../../config/Database.php';
   include_once '../../models/Categorie.php';
 
-  $categorie = new Categorie();
+  $categorie = new Categorie($id, $nom);
 
   $res = $categorie->readAllCategorie();
   if($res){
@@ -13,13 +13,3 @@
   }else{
       echo 'error';
   }
-
- 
-//   if($num >0){
-//       $res2 = $res->fetchAll(PDO::FETCH_ASSOC);
-//       echo json_encode($res2);
-//   }else{
-//       echo json_encode(
-//           array('message' => 'Aucune information à afficher')
-//       );
-//   }

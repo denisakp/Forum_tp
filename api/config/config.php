6d8b30ace9b8ea16106b66def8d8fa133836020f
denisakp <akpagnonited@gmail.com>
2019-02-14 13:23:28 +0000

    Creating a config file for DB

---
+++ api/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Fichier de configuration de la base de données.
+ * SERVER: est l'addresse du serveur
+ * USERNAME: est le nom d'utilisateur de la BDD
+ * PASSWORD: est le mot de passe de l'utilisateur de la BDD
+ * DBNAME: est le nom de la BDD 
+ */
+
+
+define ("SERVER", "localhost");
+define ("USERNAME", "error504");
+define ("PASSWORD", "admin1234");
+define ("DBNAME", "forum_db");

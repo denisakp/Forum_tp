91ec66a7c9845e04cfa6f25037fd77c82adcef8c
denisakp <akpagnonited@gmail.com>
2019-02-15 10:58:25 +0000

    Update include paths and adding $matricule to the constructor

--- api/core/etudiant/create.php
+++ api/core/etudiant/create.php
@@ -4,16 +4,17 @@
   header('Access-Control-Allow-Methods: POST');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-  include_once '../config/Database.php';
-  include_once '../models/Etudiant.php';
-  
+  include_once '../../config/Database.php';
+  include_once '../../models/Etudiant.php';
+
+$matricule = $_POST['matricule'];
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $pseudo = $_POST['pseudo'];
 $motdepasse= password_hash($_POST['motdepasse'], PASSWORD_DEFAULT);
 $filiere = $_POST['filiere'];
 
-$etudiant = new Etudiant($nom, $prenom, $pseudo, $motdepasse, $filiere);
+$etudiant = new Etudiant($matricule, $nom, $prenom, $pseudo, $motdepasse, $filiere);
 if($etudiant->addEtudiant()){
     echo 'succes';
 }else{

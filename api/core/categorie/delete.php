02a9ad6517ea65f588494ddcf977384b75741eb1
denisakp <akpagnonited@gmail.com>
2019-02-16 08:39:06 +0000

    Update delete.php code

--- api/core/categorie/delete.php
+++ api/core/categorie/delete.php
@@ -7,11 +7,12 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type
 include_once '../../config/Database.php';
 include_once '../../models/Categorie.php';
 
-$id = NULL;
-$nom = NULL;
-$categorie = new Categorie($id, $nom);
+$id = $_POST['id_cat'];
+$libelle = NULL;
 
-$res = $categorie->deleteCategorie();
+$categorie = new Categorie($id, $libele);
+
+$res = $categorie->deleteCategorie($categorie);
 if($res){
     echo "succes";
 }else{
